3f6da094d2beece8334367ba8c6822535a614df2
Chase <chase.anderson.000@gmail.com>
2022-02-20 01:45:27 -0600

    final lab2

--- and08479_ee4341_lab2.X/sourceOfFiles/and08479_main.c
+++ and08479_ee4341_lab2.X/sourceOfFiles/and08479_main.c
@@ -13,36 +13,6 @@
 #include "stdio.h"
 #include "xc.h"
 
-
-// delete later start
-
-// header defines
-
-#define CTRL_REG1       0x20
-#define CTRL_REG2       0x21
-#define CTRL_REG3       0x22
-#define CTRL_REG4       0x23
-#define CTRL_REG5       0x24
-#define CTRL_REG6       0x25
-#define REFERENCE       0x26
-#define INT1_THS        0x32
-#define INT1_DUR        0x33
-#define INT1_CFG        0x34
-#define FIFO_CTRL       0x2E
-
-#define OUT_X_L         0x28
-#define OUT_X_H         0x29
-#define OUT_Y_L         0x2A
-#define OUT_Y_H         0x2B
-#define OUT_Z_L         0x2C
-#define OUT_Z_H         0x2D
-
-// The chip select pin being used for the accelerometer
-
-#define ACCEL_CS              LATGbits.LATG9
-
-// delete later end
-
 void setup(void)
 {
     io_setup();
